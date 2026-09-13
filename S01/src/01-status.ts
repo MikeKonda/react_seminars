0374617a8008c5894e0bfd6875037fd0ddbcf428
Mihail Kondakov <makondakov@edu.hse.ru>
2026-09-13 21:06:59 +0300

    01 done

--- S01/src/01-status.ts
+++ S01/src/01-status.ts
@@ -6,12 +6,26 @@
 //    становился ошибкой компиляции.
 // 4. Добавьте в Status вариант "cancelled" и почините то, что сломается.
 
-export type Status = "idle" | "loading" | "success" | "error";
+export type Status = "idle" | "loading" | "success" | "error" | "cancelled";
 
 export function assertNever(value: never): never {
     throw new Error("Необработанный вариант: " + JSON.stringify(value));
 }
 
 export function statusMessage(status: Status): string {
-    throw new Error("не реализовано");
+    switch (status) {
+        case "idle":
+            return 'idle'
+        case "loading":
+            return 'loading';
+        case "success":
+            return 'success';
+        case "error":
+            return 'error';
+        case "cancelled":
+            return "cancelled";
+
+        default:
+            assertNever(status);
+    }
 }
